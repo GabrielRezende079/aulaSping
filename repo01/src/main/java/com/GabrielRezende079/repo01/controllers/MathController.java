c9500d4e67e989c46f08544f432a6806f4acd21a
GabrielRezende079 <grezendabarbosaflorencio@gmail.com>
2025-11-05 15:49:03 -0300

    I added the others operations and some comments

--- repo01/src/main/java/com/GabrielRezende079/repo01/controllers/MathController.java
+++ repo01/src/main/java/com/GabrielRezende079/repo01/controllers/MathController.java
@@ -27,6 +27,72 @@ public class MathController {
         return convertToDouble(numberOne)  + convertToDouble(numberTwo);
     }   
 
+    // Subtraction
+    @RequestMapping("/sub/{numberOne}/{numberTwo}")
+    public double subtract( 
+        @PathVariable("numberOne") String numberOne, 
+        @PathVariable("numberTwo") String numberTwo
+        ) throws Exception { 
+        
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+    }
+
+    // Multiplication
+    @RequestMapping("/mult/{numberOne}/{numberTwo}")
+    public double Multiplication( 
+        @PathVariable("numberOne") String numberOne, 
+        @PathVariable("numberTwo") String numberTwo
+        ) throws Exception { 
+        
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+    }
+
+    // Division
+    @RequestMapping("/div/{numberOne}/{numberTwo}")
+    public double Division(
+        @PathVariable("numberOne")String numberOne,
+        @PathVariable("numberTwo")String numberTwo
+        ) throws Exception {
+         
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+
+        }
+
+    // Avarage
+    @RequestMapping("/avarage/{numberOne}/{numberTwo}")    
+    public double avarage(
+        @PathVariable("numberOne") String numberOne, 
+        @PathVariable("numberTwo") String numberTwo
+        ) throws Exception { 
+        
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2;
+    }
+    
+    // Square Root
+    @RequestMapping("/squareRoot/{number}")
+    public double squareRoot(
+        @PathVariable("number") String number
+    ) throws Exception {
+
+        if (!isNumeric(number)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return Math.sqrt(convertToDouble(number));
+    }
+    
+
 
     // this method converts a string to a double for math operations and also handles comma as decimal separator
     private double convertToDouble(String strNumber){
--- repo01/src/main/java/com/GabrielRezende079/repo01/exception/exceptionResponse.java
+++ repo01/src/main/java/com/GabrielRezende079/repo01/exception/exceptionResponse.java
@@ -1,6 +1,7 @@
 package com.GabrielRezende079.repo01.exception;
 import java.util.Date;
 
+// This record represents the structure of an exception response
 public record exceptionResponse(Date Timestamp, String message, String details) {
     
 }
